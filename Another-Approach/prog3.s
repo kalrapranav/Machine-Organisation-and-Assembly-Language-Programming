2020c4a34a8065bcb15747b0ca97b22f8b958c31
Pranav Kalra <pranav99kalra@gmail.com>
2019-04-27 22:42:15 -0700

    prog3.s template

---
+++ Another-Approach/prog3.s
@@ -0,0 +1,47 @@
+*----------------------------------------------------------------------
+* Programmer: 
+* Class Account: 
+* Assignment or Title: 
+* Filename: 
+* Date completed:  
+*----------------------------------------------------------------------
+* Problem statement:
+* Input: 
+* Output: 
+* Error conditions tested: 
+* Included files: 
+* Method and/or pseudocode: 
+* References: 
+*----------------------------------------------------------------------
+*
+        ORG     $0
+        DC.L    $3000           * Stack pointer value after a reset
+        DC.L    start           * Program counter value after a reset
+        ORG     $3000           * Start at location 3000 Hex
+*
+*----------------------------------------------------------------------
+*
+#minclude /home/cs/faculty/riggins/bsvc/macros/iomacs.s
+#minclude /home/cs/faculty/riggins/bsvc/macros/evtmacs.s
+*
+*----------------------------------------------------------------------
+*
+* Register use
+*
+*----------------------------------------------------------------------
+*
+start:  initIO                  * Initialize (required for I/O)
+	setEVT			* Error handling routines
+*	initF			* For floating point macros only	
+
+				* Your code goes HERE
+
+
+        break                   * Terminate execution
+*
+*----------------------------------------------------------------------
+*       Storage declarations
+
+				* Your storage declarations go 
+				* HERE
+        end
